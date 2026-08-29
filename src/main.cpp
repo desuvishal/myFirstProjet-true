c9e8a54212f7b06618459bafa9a3340e8f01ca69
Vishal <desuaaradhya26@gmail.com>
2026-08-29 18:21:10 +0530

    updated with the list

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,145 @@
 #include "lib/nlohmann/json.hpp"
 
 using json = nlohmann::json;
+
 json decode_string(const std::string& encoded_value){
-     size_t colon_index = encoded_value.find(':');
-        if (colon_index != std::string::npos) {
-            std::string number_string = encoded_value.substr(0, colon_index);
-            int64_t number = std::atoll(number_string.c_str());
-            std::string str = encoded_value.substr(colon_index + 1, number);
-            return json(str);
-        }
-         else {
+    size_t colon_index = encoded_value.find(':');
+    if (colon_index == std::string::npos) {
+        throw std::runtime_error("Invalid encoded value: " + encoded_value);
+    }
+
+    std::string number_string = encoded_value.substr(0, colon_index);
+    if (number_string.empty()) {
+        throw std::runtime_error("Invalid encoded value: " + encoded_value);
+    }
+
+    for (char ch : number_string) {
+        if (!std::isdigit(static_cast<unsigned char>(ch))) {
             throw std::runtime_error("Invalid encoded value: " + encoded_value);
         }
+    }
+
+    size_t length = static_cast<size_t>(std::stoll(number_string));
+    if (colon_index + 1 + length != encoded_value.size()) {
+        throw std::runtime_error("Invalid encoded value: " + encoded_value);
+    }
+
+    return json(encoded_value.substr(colon_index + 1, length));
 }
 
 json decode_int(const std::string& encoded_value){
-    return json(std::strtoll(&encoded_value[1],nullptr,10));
+    if (encoded_value.size() < 3 || encoded_value[0] != 'i' || encoded_value.back() != 'e') {
+        throw std::runtime_error("Invalid encoded value: " + encoded_value);
+    }
+
+    std::string number_string = encoded_value.substr(1, encoded_value.size() - 2);
+    if (number_string.empty()) {
+        throw std::runtime_error("Invalid encoded value: " + encoded_value);
+    }
+
+    for (char ch : number_string) {
+        if (ch != '-' && !std::isdigit(static_cast<unsigned char>(ch))) {
+            throw std::runtime_error("Invalid encoded value: " + encoded_value);
+        }
+    }
+
+    char* end = nullptr;
+    long long value = std::strtoll(number_string.c_str(), &end, 10);
+    if (end == number_string.c_str() || *end != '\0') {
+        throw std::runtime_error("Invalid encoded value: " + encoded_value);
+    }
+
+    return json(value);
+}
+
+json decode_list(const std::string& encoded_value){
+    if (encoded_value.size() < 2 || encoded_value[0] != 'l' || encoded_value.back() != 'e') {
+        throw std::runtime_error("Invalid encoded value: " + encoded_value);
+    }
+
+    size_t index = 1;
+    json list = json::array();
+
+    while (index < encoded_value.size() && encoded_value[index] != 'e') {
+        if (encoded_value[index] == 'i') {
+            size_t end = encoded_value.find('e', index);
+            if (end == std::string::npos) {
+                throw std::runtime_error("Invalid encoded value: " + encoded_value);
+            }
+            std::string token = encoded_value.substr(index, end - index + 1);
+            list.push_back(decode_int(token));
+            index = end + 1;
+        }
+        else if (std::isdigit(static_cast<unsigned char>(encoded_value[index]))) {
+            size_t colon_index = encoded_value.find(':', index);
+            if (colon_index == std::string::npos) {
+                throw std::runtime_error("Invalid encoded value: " + encoded_value);
+            }
+
+            std::string number_string = encoded_value.substr(index, colon_index - index);
+            for (char ch : number_string) {
+                if (!std::isdigit(static_cast<unsigned char>(ch))) {
+                    throw std::runtime_error("Invalid encoded value: " + encoded_value);
+                }
+            }
+
+            size_t length = static_cast<size_t>(std::stoll(number_string));
+            size_t token_length = (colon_index - index) + 1 + length;
+            if (index + token_length > encoded_value.size()) {
+                throw std::runtime_error("Invalid encoded value: " + encoded_value);
+            }
+
+            std::string token = encoded_value.substr(index, token_length);
+            list.push_back(decode_string(token));
+            index += token_length;
+        }
+        else if (encoded_value[index] == 'l') {
+            size_t nested_start = index;
+            size_t depth = 1;
+            ++index;
+
+            while (index < encoded_value.size() && depth > 0) {
+                if (encoded_value[index] == 'l') {
+                    ++depth;
+                }
+                else if (encoded_value[index] == 'e') {
+                    --depth;
+                }
+                ++index;
+            }
+
+            if (depth != 0) {
+                throw std::runtime_error("Invalid encoded value: " + encoded_value);
+            }
+
+            std::string token = encoded_value.substr(nested_start, index - nested_start);
+            list.push_back(decode_list(token));
+        }
+        else {
+            throw std::runtime_error("Invalid encoded value: " + encoded_value);
+        }
+    }
+
+    if (index != encoded_value.size() - 1 || encoded_value[index] != 'e') {
+        throw std::runtime_error("Invalid encoded value: " + encoded_value);
+    }
+
+    return list;
 }
 
 json decode_bencoded_value(const std::string& encoded_value) {
-    if (std::isdigit(encoded_value[0])) {
+    if (encoded_value.empty()) {
+        throw std::runtime_error("Invalid encoded value: " + encoded_value);
+    }
+
+    if (std::isdigit(static_cast<unsigned char>(encoded_value[0]))) {
         return decode_string(encoded_value);
-    } 
-    else if(encoded_value[0] == 'i' && encoded_value.back() == 'e'){
+    }
+    else if (encoded_value[0] == 'i') {
         return decode_int(encoded_value);
+    }
+    else if (encoded_value[0] == 'l') {
+        return decode_list(encoded_value);
     }
     else {
         throw std::runtime_error("Unhandled encoded value: " + encoded_value);
@@ -37,7 +153,7 @@ json decode_bencoded_value(const std::string& encoded_value) {
 }
 
 int main(int argc, char* argv[]) {
-    // Flush after every std::cout / std::cerr
+
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
